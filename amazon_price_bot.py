eb3a46fe8f35e7e16020840ad821a1d25066df45
wokaya61-cyber <wokaya61@gmail.com>
2026-05-03 06:44:17 +0300

    fix user agent

--- amazon_price_bot.py
+++ amazon_price_bot.py
@@ -131,7 +131,7 @@ def get_product_info(url):
         options.add_argument("--disable-dev-shm-usage")
 
         options.add_argument(
-            "user-agent=Mozilla/5.0"
+            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
         )
 
         driver = uc.Chrome(

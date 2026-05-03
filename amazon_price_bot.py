0efdcad34ac720ef7b62b53efb8549d8e5cb14f9
wokaya61-cyber <wokaya61@gmail.com>
2026-05-03 07:07:14 +0300

    switch selenium

--- amazon_price_bot.py
+++ amazon_price_bot.py
@@ -4,8 +4,10 @@
 # AUTO CLEAN AMAZON LINKS
 # =========================================
 
-import undetected_chromedriver as uc
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import json
 import os
 import re
@@ -127,14 +129,13 @@ def get_product_info(url):
         options.add_argument("--disable-dev-shm-usage")
 
         options.add_argument(
-            "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+        "user-agent=Mozilla/5.0 ..."
         )
 
         driver = uc.Chrome(
             options=options,
             use_subprocess=True
         )
-
 
         driver.get(url)
 
